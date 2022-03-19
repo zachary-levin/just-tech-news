30ac4eddc8d6e9df16b595a98e2fb11c9fac18ee
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-03-19 00:12:26 -0500

    create a login and signup page

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -63,11 +63,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
 });
 
 router.post('/login', (req, res) => {
@@ -91,8 +95,27 @@ router.post('/login', (req, res) => {
           return;
       }
 
+      req.session.save(() => {
+          // declare session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+      
       res.json({ user: dbUserData, message: 'You are now logged in!' });
   });
+ });
+});
+
+// Log out of session
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
